a216b47b4f21a944b4fef408697732fc9e32cb32
glitch003 <glitch003@users.noreply.github.com>
2024-11-22 13:43:03 +0000

    deploy: b0d361e8702ac98c66055657b64bd83162755f8c

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJKSU1LLM0pUdJRKkgsyVCyUsrNTynNSS3WyyjJzVFGyGZn5qUoWZkYWpoYG5jU6sD1+2SW+BclJuekemciGZKck1hcnFqsjywLNhFukqGRRW0sAN07A/aHAAAA"
